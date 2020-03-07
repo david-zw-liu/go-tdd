85f5b475fc401f92bb472d2bc2bd64a88ba48032
David Liu <msglight4874@gmail.com>
2020-03-07 17:34:02 +0800

    Implement `SumAll` function

--- 04_array_and_slices/sum.go
+++ 04_array_and_slices/sum.go
@@ -8,3 +8,13 @@ func Sum(numbers []int) int {
 
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+	for index, numbers := range numbersToSum {
+		sums[index] = Sum(numbers)
+	}
+
+	return sums
+}
