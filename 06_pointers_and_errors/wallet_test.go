0a5d15192b3cb4ce2067c7f5c61b0dceabc9a2fe
David Liu <msglight4874@gmail.com>
2020-03-07 19:55:40 +0800

    Move assetion functions to ouside

--- 06_pointers_and_errors/wallet_test.go
+++ 06_pointers_and_errors/wallet_test.go
@@ -2,28 +2,35 @@ package main
 
 import "testing"
 
+func assertBalance(t *testing.T, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertError(t *testing.T, got error, want error) {
+	t.Helper()
+
+	if got == nil {
+		t.Error("wanted an error but didn't get one")
+	}
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func assertNoError(t *testing.T, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
+	}
+}
+
 func TestWallet(t *testing.T) {
-	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
-		t.Helper()
-		got := wallet.Balance()
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	}
-
-	assertError := func(t *testing.T, got error, want error) {
-		t.Helper()
-
-		if got == nil {
-			t.Error("wanted an error but didn't get one")
-		}
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	}
-
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
@@ -33,8 +40,9 @@ func TestWallet(t *testing.T) {
 
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
 
+		assertNoError(t, err)
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/msglight4874/go-tdd
 
 go 1.14
+
+require (
+	github.com/kisielk/errcheck v1.2.0 // indirect
+	golang.org/x/tools v0.0.0-20200306191617-51e69f71924f // indirect
+)
